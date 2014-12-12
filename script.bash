e4daec789bfdc6584d71fbd2edb542fe5bc89ef9
Matti Lankinen <m.lankinen@iki.fi>
2014-12-12 20:06:17 +0200

    Silent output

--- script.bash
+++ script.bash
@@ -5,7 +5,7 @@ REMOTES=`git remote`
 
 while true; do
   for remote in $REMOTES; do
-    git add . && git commit -am "Some changes" && git push -f $remote $BRANCH > /dev/null 2>&1
+    $(git add . && git commit -am "Some changes" && git push -f $remote $BRANCH) > /dev/null 2>&1
   done
   sleep 1
 done
